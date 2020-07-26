2bf1985031eb47681fad15e0cbd538dc4f8658cf
Koleta19 <67999358+Koleta19@users.noreply.github.com>
2020-07-26 18:43:56 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
 let posX = 0
 let posY = 3
 let direction = 1
+let liste = [0, 1, 2, 3, 4]
 led.plot(posX, posY)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     led.unplot(posX, posY)
-    direction += direction
+    posY += direction
     led.plot(posX, posY)
     if (posY >= 4) {
         direction = -1
     } else if (posY <= 0) {
         direction = 1
+    } else if (0 == 0) {
+        posX = 3
+    } else if (0 == 0) {
+    	
     }
     basic.pause(1000)
 })

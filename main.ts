632b75fa897ea7a8f8d7bd40c9ffa145290e2542
Koleta19 <67999358+Koleta19@users.noreply.github.com>
2020-07-26 15:56:21 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
-let liste = [3, 4, 3, 2, 1]
-let liste_de_textes = ["a", "b", "c", "d", "e"]
-basic.showIcon(IconNames.Yes)
+let posX = 0
+let posY = 3
+let direction = 1
+led.plot(posX, posY)
 basic.forever(function () {
-	
+    led.unplot(posX, posY)
+    direction += direction
+    led.plot(posX, posY)
+    if (posY >= 4) {
+        direction = -1
+    } else if (posY <= 0) {
+        direction = 1
+    }
+    basic.pause(1000)
 })
